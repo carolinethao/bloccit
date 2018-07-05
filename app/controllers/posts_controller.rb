7bc48398fe97df326f0809e53dccb12f832846b8
Caroline Nguyen <caroline.nguyen@outlook.com>
2018-07-04 22:55:44 -0500

    a17 - completed

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,11 @@
 class PostsController < ApplicationController
   def index
     @posts = Post.all
+    @posts.each_with_index do |posts, index|
+      if index % 5 == 0
+        posts.title = "SPAM"
+      end
+    end
   end
 
   def show
