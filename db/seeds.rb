589a910c44f27644f59fa1d172f01c7639e13fa7
Caroline Nguyen <caroline.nguyen@outlook.com>
2018-08-09 12:19:17 -0500

    c27 - completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -33,9 +33,16 @@ posts = Post.all
   )
 end
 
-user = User.first
-user.update_attributes!(
-  email: 'youremail.com', # replace this with your personal email
+admin = User.create!(
+  name:     'Admin User',
+  email:    'admin@example.com',
+  password: 'helloworld',
+  role:     'admin'
+)
+
+member = User.create!(
+  name:     'Member User',
+  email:    'member@example.com',
   password: 'helloworld'
 )
 
