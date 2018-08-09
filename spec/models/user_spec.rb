2ecc9ddad62dd84198bc8ba9046d1fc53edc213b
Caroline Nguyen <caroline.nguyen@outlook.com>
2018-08-09 15:05:59 -0500

    a27 - completed

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -30,6 +30,10 @@ RSpec.describe User, type: :model do
       expect(user).to respond_to(:admin?)
     end
 
+    it "responds to moderator?" do
+      expect(user).to respond_to(:moderator?)
+    end
+
     it "responds to member?" do
       expect(user).to respond_to(:member?)
     end
@@ -43,6 +47,10 @@ RSpec.describe User, type: :model do
     context "member user" do
       it "returns true for #member?" do
         expect(user.member?).to be_truthy
+      end
+
+      it "returns false for #moderator?" do
+        expect(user.moderator?).to be_falsey
       end
 
       it "returns false for #admin?" do
@@ -59,8 +67,30 @@ RSpec.describe User, type: :model do
         expect(user.member?).to be_falsey
       end
 
+      it "returns false for #moderator?" do
+        expect(user.moderator?).to be_falsey
+      end
+
       it "returns true for #admin?" do
         expect(user.admin?).to be_truthy
+      end
+    end
+
+    context "moderator user" do
+      before do
+        user.moderator!
+      end
+
+      it "returns false for #member?" do
+        expect(user.member?).to be_falsey
+      end
+
+      it "returns false for #moderator?" do
+        expect(user.moderator?).to be_truthy
+      end
+
+      it "returns true for #admin?" do
+        expect(user.admin?).to be_falsey
       end
     end
   end
